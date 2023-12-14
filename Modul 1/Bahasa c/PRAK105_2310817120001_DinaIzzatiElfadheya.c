95a977322c621de7031e5a4312a445470cb00535
Dina Izzati Elfadheya <145301696+dinzzti@users.noreply.github.com>
2023-12-14 23:37:35 +0800

    Update PRAK105_2310817120001_DinaIzzatiElfadheya.c

--- Modul 1/Bahasa c/PRAK105_2310817120001_DinaIzzatiElfadheya.c
+++ Modul 1/Bahasa c/PRAK105_2310817120001_DinaIzzatiElfadheya.c
@@ -9,13 +9,10 @@ int main() {
     int F = C%D; 
     int G = E+F;
 
-
     printf (" Variabel a bernilai %d\n", A);
     printf (" Variabel b bernilai %d\n", B);
     printf (" Variabel x bernilai %d\n", C);
     printf (" Variabel a bernilai %d\n", D);
     printf ("Total sisa bagi dari a dibagi b dan x dibagi y adalah %d\n", G);
-
-
-
+    return0;
 }
