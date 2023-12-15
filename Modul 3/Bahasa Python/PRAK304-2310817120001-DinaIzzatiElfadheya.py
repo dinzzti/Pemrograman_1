67566e068b801ebed9679e8105760b1e4f1c497b
Dina Izzati Elfadheya <145301696+dinzzti@users.noreply.github.com>
2023-12-15 20:41:32 +0800

    Update PRAK304-2310817120001-DinaIzzatiElfadheya.py

--- Modul 3/Bahasa Python/PRAK304-2310817120001-DinaIzzatiElfadheya.py
+++ Modul 3/Bahasa Python/PRAK304-2310817120001-DinaIzzatiElfadheya.py
@@ -2,13 +2,13 @@ print("Input")
 bilangan = (int(input()))
 
 print("\nOutput")
-if bilangan <100 and bilangan >=20: # bilangan puluhan
+if bilangan <100 and bilangan >=20:
   print("Puluhan")
-elif bilangan <20 and bilangan >=10:  # bilangan belasan
+elif bilangan <20 and bilangan >=10:  
   print("Belasan")
-elif bilangan >0 and bilangan <=9: # bilangan satuann
+elif bilangan >0 and bilangan <=9: 
   print("Satuan")  
-elif bilangan == 0 : # nol
+elif bilangan == 0 :
   print("Nol")
 else:
   print("Anda Menginput Melebihi Limit Bilangan")
