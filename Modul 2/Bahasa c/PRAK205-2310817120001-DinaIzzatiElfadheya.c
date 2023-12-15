b18cf0f8fee50611dacbdf90cd75843bac8ada59
Dina Izzati Elfadheya <145301696+dinzzti@users.noreply.github.com>
2023-12-15 19:39:20 +0800

    Update PRAK205-2310817120001-DinaIzzatiElfadheya.c

--- Modul 2/Bahasa c/PRAK205-2310817120001-DinaIzzatiElfadheya.c
+++ Modul 2/Bahasa c/PRAK205-2310817120001-DinaIzzatiElfadheya.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<math.h>
 void main(){
-    int A, B, C, D, E, F, G; //40 41 | 16 65
+    int A, B, C, D, E, F, G; 
     printf("Input\n");
     scanf("%i", &A);
     printf("");
     scanf("%i", &B);
 
     C= pow(B,2)-pow(A,2);
-    D= sqrt(C);//alas
-    E= (int)A+B+D;//keliling
-    F= (int)A*D/2;//luas
+    D= sqrt(C);
+    E= (int)A+B+D;
+    F= (int)A*D/2;
 
     printf("\nOutput\n");
     printf("Alas = %i cm \n", D );
